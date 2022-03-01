e9925180e3771b2767a7c14bdc61292a61781de3
ich777 <christophhummer@gmail.com>
2022-03-01 18:32:02 +0100

    „scripts/start.sh“ ändern

--- scripts/start.sh
+++ scripts/start.sh
@@ -21,8 +21,8 @@ chmod -R 750 /opt/scripts
 chown -R ${UID}:${GID} ${DATA_DIR}
 
 term_handler() {
-	kill -SIGTERM "$killpid"
-	wait "$killpid" -f 2>/dev/null
+	kill -SIGINT $(pidof LongvinterServer-Linux-Shipping)
+	tail --pid=$(pidof LongvinterServer-Linux-Shipping) -f 2>/dev/null
 	exit 143;
 }
 
