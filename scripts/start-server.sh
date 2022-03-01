62d3ac602eba26ede162663aac342fed1e875cfd
ich777 <christophhummer@gmail.com>
2022-03-01 18:22:33 +0100

    „scripts/start-server.sh“ ändern

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,73 +1,58 @@
 #!/bin/bash
 if [ ! -f ${STEAMCMD_DIR}/steamcmd.sh ]; then
-    echo "SteamCMD not found!"
-    wget -q -O ${STEAMCMD_DIR}/steamcmd_linux.tar.gz http://media.steampowered.com/client/steamcmd_linux.tar.gz 
-    tar --directory ${STEAMCMD_DIR} -xvzf /serverdata/steamcmd/steamcmd_linux.tar.gz
-    rm ${STEAMCMD_DIR}/steamcmd_linux.tar.gz
+  echo "SteamCMD not found!"
+  wget -q -O ${STEAMCMD_DIR}/steamcmd_linux.tar.gz http://media.steampowered.com/client/steamcmd_linux.tar.gz 
+  tar --directory ${STEAMCMD_DIR} -xvzf /serverdata/steamcmd/steamcmd_linux.tar.gz
+  rm ${STEAMCMD_DIR}/steamcmd_linux.tar.gz
 fi
 
 echo "---Update SteamCMD---"
 if [ "${USERNAME}" == "" ]; then
-    ${STEAMCMD_DIR}/steamcmd.sh \
-    +login anonymous \
-    +quit
+  ${STEAMCMD_DIR}/steamcmd.sh \
+  +login anonymous \
+  +quit
 else
-    ${STEAMCMD_DIR}/steamcmd.sh \
-    +login ${USERNAME} ${PASSWRD} \
-    +quit
+  ${STEAMCMD_DIR}/steamcmd.sh \
+  +login ${USERNAME} ${PASSWRD} \
+  +quit
 fi
 
-echo "---Sleep zZzZz---"
-sleep infinity
-
 echo "---Update Server---"
-if [ "${USERNAME}" == "" ]; then
-    if [ "${VALIDATE}" == "true" ]; then
-    	echo "---Validating installation---"
-        ${STEAMCMD_DIR}/steamcmd.sh \
-        +force_install_dir ${SERVER_DIR} \
-        +login anonymous \
-        +app_update ${GAME_ID} validate \
-        +quit
-    else
-        ${STEAMCMD_DIR}/steamcmd.sh \
-        +force_install_dir ${SERVER_DIR} \
-        +login anonymous \
-        +app_update ${GAME_ID} \
-        +quit
-    fi
+cd ${SERVER_DIR}
+if [ ! -f ${SERVER_DIR}/Longvinter/Binaries/Linux/LongvinterServer-Linux-Shipping ]; then
+  git clone https://github.com/Uuvana-Studios/longvinter-linux-server.git ${SERVER_DIR}
+elif git merge-base --is-ancestor origin/main main ; then
+  echo "---Nothing to do, game up to date!---"
 else
-    if [ "${VALIDATE}" == "true" ]; then
-    	echo "---Validating installation---"
-        ${STEAMCMD_DIR}/steamcmd.sh \
-        +force_install_dir ${SERVER_DIR} \
-        +login ${USERNAME} ${PASSWRD} \
-        +app_update ${GAME_ID} validate \
-        +quit
-    else
-        ${STEAMCMD_DIR}/steamcmd.sh \
-        +force_install_dir ${SERVER_DIR} \
-        +login ${USERNAME} ${PASSWRD} \
-        +app_update ${GAME_ID} \
-        +quit
-    fi
+  echo "---Updating game, please wait!---"
+  git restore
+  sleep 1
+  git pull "https://github.com/Uuvana-Studios/longvinter-linux-server.git" main ${SERVER_DIR}
 fi
 
 echo "---Prepare Server---"
-if [ ! -f ${DATA_DIR}/.steam/sdk32/steamclient.so ]; then
-	if [ ! -d ${DATA_DIR}/.steam ]; then
-    	mkdir ${DATA_DIR}/.steam
-    fi
-	if [ ! -d ${DATA_DIR}/.steam/sdk32 ]; then
-    	mkdir ${DATA_DIR}/.steam/sdk32
-    fi
-    cp -R ${STEAMCMD_DIR}/linux32/* ${DATA_DIR}/.steam/sdk32/
+if [ ! -f ${DATA_DIR}/.steam/sdk64/steamclient.so ]; then
+  if [ ! -d ${DATA_DIR}/.steam/sdk64 ]; then
+    mkdir -p ${DATA_DIR}/.steam/sdk64
+  fi
+  cp -R ${STEAMCMD_DIR}/linux64/* ${DATA_DIR}/.steam/sdk64/
+fi
+if [ ! -f ${SERVER_DIR}/Longvinter/Saved/Config/LinuxServer/Game.ini ]; then
+    echo "---No config file found, creating...---"
+    echo "[/Game/Blueprints/Server/GI_AdvancedSessions.GI_AdvancedSessions_C]
+ServerName=Longvinter Docker
+MaxPlayers=32
+ServerMOTD=Welcome to Longvinter running in Docker!
+Password=
+CommunityWebsite=unraid.net
+
+[/Game/Blueprints/Server/GM_Longvinter.GM_Longvinter_C]
+AdminSteamID=" > ${SERVER_DIR}/Longvinter/Saved/Config/LinuxServer/Game.ini
 fi
 chmod -R ${DATA_PERM} ${DATA_DIR}
+chmod +x ${SERVER_DIR}/Longvinter/Binaries/Linux/LongvinterServer-Linux-Shipping
 echo "---Server ready---"
-
-
 
 echo "---Start Server---"
 cd ${SERVER_DIR}
-${SERVER_DIR}/srcds_run -game ${GAME_NAME} ${GAME_PARAMS} -console +port ${GAME_PORT}
+${SERVER_DIR}/Longvinter/Binaries/Linux/LongvinterServer-Linux-Shipping Longvinter ${GAME_PARAMS}
